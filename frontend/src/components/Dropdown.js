281769b8f763aee20566873beedd5fe545d23bc9
Davis Djaja <jdn1043@autuni.ac.nz>
2021-09-22 12:19:15 +1200

    front end modification

--- frontend/src/components/Dropdown.js
+++ frontend/src/components/Dropdown.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SEPractices from "../dummydata/SEPractices"
 
-  const optionItems = SEPractices.map((SEPractice) =>
-                <option key={SEPractice.practice}>{SEPractice.practice}</option>
-            );
-  const Dropdown = () => {
-    return (
-        <div>
-             <select>
-                {optionItems}
-             </select>
-         </div>
+const optionItems = SEPractices.map((SEPractice) => (
+  <option key={SEPractice.practice}>{SEPractice.practice}</option>
+));
+const Dropdown = (props) => {
+  const [selected, setSelected] = useState('');
+  const changeHandler = (option) => {
+      setSelected(option);
+      props.parentCallback(option);
+  };
 
-    )
-  }
-  export default Dropdown;
+  return (
+      <div>
+          <select onChange={(e) => changeHandler(e.target.value)}>
+              <option value="">Select an SE Practice </option>;{optionItems}
+          </select>
+      </div>
+  );
+};
+export default Dropdown;
