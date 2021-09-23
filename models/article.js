cae591bb2ec85e10e0c4c4f9ccf5efed0f122a59
Davis Djaja <jdn1043@autuni.ac.nz>
2021-09-23 23:35:13 +1200

    bug

--- models/article.js
+++ models/article.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const articleSchema = new mongoose.Schema({
-    //
+    
     _id: mongoose.Schema.Types.ObjectId,
     title: String,
     authors: String,
